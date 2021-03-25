3429e95b9d84986387578746594931cbaebc0b6f
amit328 <51810663+amit328@users.noreply.github.com>
2021-03-25 15:19:07 +0530

    image and assets

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-    title: Text('My first app'),
-    centerTitle: true,
-  ),
-  body: Center(
-    child:Text('hellooooo'),
-  ),
-  floatingActionButton: FloatingActionButton(
-    child:Text('click') ,
-  ),
-  )));
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('assets/photo-1505506874110-6a7a69069a08.jpg'),
+          // image:NetworkImage('https://images.unsplash.com/photo-1531306728370-e2ebd9d7bb99?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80')
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click '),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
 }
