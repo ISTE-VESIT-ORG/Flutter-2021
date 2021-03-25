73954793b14dffe39e52c7e89ff1f7ca150b055a
amit328 <51810663+amit328@users.noreply.github.com>
2021-03-25 18:22:22 +0530

    container and padding

--- lib/main.dart
+++ lib/main.dart
@@ -15,43 +15,12 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(
-        // child: Icon(
-        //     Icons.airport_shuttle,
-        //     color: Colors.lightBlue,
-        //     size:50.0,
-        // ),
-      //   child: ElevatedButton(
-      //     onPressed: () {
-      //       print('you clicked me');
-      //     },
-      //     child: Text('click me'),
-      //     style: ElevatedButton.styleFrom(
-      //       primary: Colors.red,
-      //       onPrimary: Colors.white,
-      //     ),
-      //   ),
-      // ),
-      // child:ElevatedButton.icon(
-      //   onPressed: (){
-
-      //   },
-      //   icon: Icon(
-      //     Icons.mail
-      //   ),
-      //   label: Text('mail me'),
-      //   style: ElevatedButton.styleFrom(
-      //       primary: Colors.red,
-      //       onPrimary: Colors.white,
-      //     ),
-      // ),
-      child:IconButton(
-        onPressed: (){
-            print('you clicked me');
-        },
-        icon: Icon(Icons.alternate_email),
-        color: Colors.red,
-      )
+      body: Container(
+        //padding: EdgeInsets.symmetric(20.0),
+        padding: EdgeInsets.symmetric(horizontal: 30.0,vertical: 10.0),
+        margin: EdgeInsets.all(30.0),
+        color:Colors.grey[400],
+        child: Text('hello'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
