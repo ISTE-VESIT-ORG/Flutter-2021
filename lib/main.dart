f4270761826328c6741c07b1dc03a2141656f38d
amit328 <51810663+amit328@users.noreply.github.com>
2021-03-25 15:24:36 +0530

    stateless

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      home: Scaffold(
+    home: Home(),
+  ));
+      
+
+  
+}
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
           appBar: AppBar(
     title: Text('My first app'),
     centerTitle: true,
+    backgroundColor:Colors.red[600],
   ),
   body: Center(
-    child:Text('hellooooo'),
+    child:Text('hellooooo',
+    style: TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+      letterSpacing: 2.0,
+      color: Colors.grey[600],
+      fontFamily: 'Roboto',
+
+    ),),
   ),
   floatingActionButton: FloatingActionButton(
-    child:Text('click') ,
+    onPressed: (){},
+    child:Text('click ') ,
+    backgroundColor:Colors.red[600],
   ),
-  )));
+  );
+  }
 }
