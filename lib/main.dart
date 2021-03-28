e2afb35ab83ee1165fdd1697027c9f8d70a0dac7
amit328 <51810663+amit328@users.noreply.github.com>
2021-03-28 10:56:43 +0530

    app Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:start/quote.dart';
+import 'package:start/quote_card.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-    title: Text('My first app'),
-    centerTitle: true,
-  ),
-  body: Center(
-    child:Text('hellooooo'),
-  ),
-  floatingActionButton: FloatingActionButton(
-    child:Text('click') ,
-  ),
-  )));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+
+        primarySwatch: Colors.blue,
+      ),
+      home: QuoteList(),
+    );
+  }
+}
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(author: 'No one can make you feel inferior without your consent.', text: 'Eleanor Roosevelt'),
+    Quote(author: 'Not all those who wander are lost.', text: 'J. R. R. Tolkein'),
+    Quote(author: 'A rose by any other name would smell as sweet.	', text: 'William Shakespeare'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) {
+          return QuoteCard(
+              quote: quote,
+              delete: () {
+                setState(() {
+                  quotes.remove(quote);
+                });
+              });
+        }).toList(),
+      ),
+    );
+  }
 }
